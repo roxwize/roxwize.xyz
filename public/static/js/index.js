ae38a567fc50d1318c8d96024b2e0e4af12c919b
roxwize <rx@roxwize.xyz>
2024-09-13 23:38:53 -0400

    new homepage YAY

---
+++ public/static/js/index.js
@@ -0,0 +1,30 @@
+const lines = [
+  ["teenage", "programming", "scum"], // ARTIFICIAL PROBABILITY INFLATION
+  ["teenage", "programming", "scum"],
+  ["teenage", "programming", "scum"],
+  ["teenage", "programming", "scum"],
+  ["teenage", "programming", "scum"],
+  ["teenage", "programming", "scum"],
+  ["teenage", "programming", "scum"],
+  ["teenage", "programming", "scum"],
+  ["it's whatever", "you say", "it is"],
+  ["in", "split", "infinity"],
+  ["this", "must", "be"],
+  ["mobius", "double", "reacharound"],
+  ["time", "truth", "and hearts"],
+  ["my existence", "is a momentary", "lapse of reason"],
+  ["before", "you're", "comatose"],
+  ["the past", "inside", "the present"],
+  ["times", "the", "raker"],
+  ["all", "is", "well"],
+  ["like an", "apple split", "in two"],
+  ["it all", "returns", "to nothing"],
+  ["minn besti", "vinur hverju", "sem dynur"],
+  ["turn it on", "tune it in", "and stay inert"],
+  ["this", "dread", "circumference"],
+  ["like", "spinning", "plates"]
+]
+
+const line = lines[Math.floor(Math.random() * lines.length)];
+document.getElementById("splashtop").innerHTML = line[0];
+document.getElementById("splashbtm").innerHTML = line.slice(1).join("<br>");
