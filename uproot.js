885cb0d7f4d657ec2cd62b0ca2a46711f98e2785
roxwize <rae@roxwize.xyz>
2024-11-02 21:46:42 -0400

    make music css slightly less shit
    Signed-off-by: roxwize <rae@roxwize.xyz>

--- uproot.js
+++ uproot.js
@@ -155,6 +155,8 @@ async function getPalettes() {
     }
     if (theme.type === "dark") {
       styles[".decsinc-logo"] = ["filter: invert();"]
+    } else {
+      styles[".release-main, .release-listen-date"] = ["color: var(--bg);"]
     }
     // extra options
     styles["a:not(.nu)"].push(
