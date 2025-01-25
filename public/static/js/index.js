3d01ff69d9bd25e5419d295e44b6a3512b514326
roxwize <rae@roxwize.xyz>
2025-01-25 16:26:42 -0500

    uhh
    Signed-off-by: roxwize <rae@roxwize.xyz>

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -13,9 +13,16 @@ const lines = [
   ["teenage", "programming", "scum"],
   ["teenage", "programming", "scum"],
   ["teenage", "programming", "scum"],
+  ["teenage", "programming", "scum"],
+  ["teenage", "programming", "scum"],
+  ["teenage", "programming", "scum"],
+  ["teenage", "programming", "scum"],
+  ["teenage", "programming", "scum"],
+  ["teenage", "programming", "scum"],
+  ["teenage", "programming", "scum"],
+  ["teenage", "programming", "scum"],
   ["it's whatever", "you say", "it is"],
   ["in", "split", "infinity"],
-  ["this", "must", "be"],
   ["mobius", "double", "reacharound"],
   ["time", "truth", "and hearts"],
   ["my existence", "is a momentary", "lapse of reason"],
@@ -29,18 +36,23 @@ const lines = [
   ["turn it on", "tune it in", "and stay inert"],
   ["this", "dread", "circumference"],
   ["like", "spinning", "plates"],
-  ["same", "as", "above"],
-  ["you'll catch", "a jpeg", "to the head"],
+  ["you'll catch", "a jpeg to", "the head"],
   ["i", "love", "rivulet"],
   ["now", "i can", "hibernate!"],
-  ["fast", "frustrating", "fun"],
-  ["a looking", "around", "simulator"],
   ["meow", "meow", "meow"],
-  ["got the", "dna of", "gothic lemons"],
   ["mountains", "beyond", "mountains"],
-  ["i'm moving", "past the", "feeling"]
+  ["i'm moving", "past the", "feeling"],
+  ["the more", "you try to", "erase me"],
+  ["segmentation", "fault (core", "dumped)"],
+  ["LOL!", "it's the", "mammoth!"],
+  ["subscribe to dsl", "reboot your email", "install a window", "replace your mouse"],
+  ["fast", "fluid", "frustrating", "fun"], // this feels like an innuendo
+  ["pinky and", "pepper forever", "and ever"],
+  ["hold", "your", "color"],
+  ["we were", "fated to", "pretend"],
+  ["no need to ask", "my name to figure", "out how cool i am"]
 ]
 
 const line = lines[Math.floor(Math.random() * lines.length)];
-document.getElementById("splashtop").innerHTML = line[0];
-document.getElementById("splashbtm").innerHTML = line.slice(1).join("<br>");
+document.getElementById("flavortext").innerHTML = line.slice(0, 3).join("<br>");
+if (line[3]) document.getElementById("etc").innerHTML = line[3];
